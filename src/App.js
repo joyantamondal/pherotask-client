64e2a580031ded18fc3720dfcd545cb484ec6d10
Joyanta Mondal <engr.joyantamondal@gmail.com>
2022-07-01 12:23:19 +0600

    modal data post

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AddBillModal from "./Pages/Home/AddBillModal/AddBillModal";
 import Home from "./Pages/Home/Home/Home";
+import SearchBar from "./Pages/Home/SearchBar/SearchBar";
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />}/>
-      <Route path="/add-billing" element={<AddBillModal />}/>
+      <Route path="/add-billing" element={<SearchBar />}/>
        
     </Routes>
   </BrowserRouter>
